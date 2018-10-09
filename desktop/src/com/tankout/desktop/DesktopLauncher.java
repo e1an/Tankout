a4a4a3f3450e38ac85826eafc2821bd90b1b39a6
Evan <evanjewell@me.com>
2018-10-08 23:58:11 -0500

    Initial commit

---
+++ desktop/src/com/tankout/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tankout.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tankout.Tankout;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Tankout(), config);
+	}
+}
